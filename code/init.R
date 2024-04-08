1b52329cc1a184779e16912be4096a649298d4e8
Emma <emma.kopp@dauphine.psl.eu>
2024-04-08 17:21:48 +0200

    clean code

---
+++ code/init.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(readxl)
+library(phangorn)
+library(TreeTools)
+library(dplyr)
+library(ape)
+library(phytools)
+
---
+++ code/weight_db.R
@@ -0,0 +1,18 @@
+rm(list=ls())
+
+# Import file 
+library(here)
+source(here("code/functions.R"))
+source(here("code/init.R"))
+
+
+# Path to data
+path = here("data/Kra-DaiLooms28master-2.xlsx")
+
+# Create nexus file of each levels
+kd_by_level(path)
+
+# Create dataframes of each levels
+kd_by_level_df(path)
+kd_xl_weighted = duplicate_looms(c(4,1,1,1))
+data_to_nexus(kd_xl_weighted,here("test.nex"))
