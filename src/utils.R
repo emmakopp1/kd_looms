3e27cc99f77134c7ee049cda1f3976587ee0b8c1
Emma <emma.kopp@dauphine.psl.eu>
2024-06-05 17:24:33 +0200

    path on utils

--- src/utils.R
+++ src/utils.R
@@ -1,5 +1,4 @@
 library(here)
-source(here("src/init.R"))
 library(ape)
 
 remove_burnin = function(trees,burnin_rate){
@@ -16,29 +15,29 @@ myconsensus = function(trees){
 }
 
 # Weigth 1111 : Burnin 90%
-path1111 = "/Users/kopp/Documents/kd_looms/output/by_level/loom1111/kd_loom1111.trees"
+path1111 = here("output/by_level/loom1111/kd_loom1111.trees")
 trees1111 = read.nexus(path1111)
 trees1111 = remove_burnin(trees1111,0.9)
 consensus_tree1111 = myconsensus(trees1111)
 plot(consensus_tree1111,main='1111')
-write.tree(consensus_tree1111,here("output/by_level/loom1111/consensus_tree1111.tree"))
+write.tree(consensus_tree1111,here(here("output/by_level/loom1111/consensus_tree1111.tree")))
 
 
 # Weigth 8421 : Burnin 90%
-path8421 = "/Users/kopp/Documents/kd_looms/output/by_level/loom8421/kd_loom8421.trees"
+path8421 = here("output/by_level/loom8421/kd_loom8421.trees")
 trees8421 = read.nexus(path8421)
 trees8421 = remove_burnin(trees8421,0.9)
 consensus_tree8421 = myconsensus(trees8421)
 plot(consensus_tree8421,main='8421')
-write.tree(consensus_tree1111,here("output/by_level/loom8421/consensus_tree8421.tree"))
+write.tree(consensus_tree1111,here(here("output/by_level/loom8421/consensus_tree8421.tree")))
 
 # Weigth 1000 : Burnin 90%
-path1000 = "/Users/kopp/Documents/kd_looms/output/by_level/loom1000/kd_loom1000.trees"
+path1000 = here("output/by_level/loom1000/kd_loom1000.trees")
 trees1000 = read.nexus(path1000)
 trees1000 = remove_burnin(trees1000,0.9)
 consensus_tree1000 = myconsensus(trees1000)
 plot(consensus_tree1000,main='1000')
-write.tree(consensus_tree1000,here("output/by_level/loom1000/consensus_tree1000.tree"))
+write.tree(consensus_tree1000,here(here("output/by_level/loom1000/consensus_tree1000.tree")))
 
 
 
