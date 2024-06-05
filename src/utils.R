849a7c880251c192a3e21f6397c45439f33c8abd
Emma <emma.kopp@dauphine.psl.eu>
2024-06-05 17:28:37 +0200

    bug

--- src/utils.R
+++ src/utils.R
@@ -17,7 +17,7 @@ trees1111 <- read.nexus(path1111)
 trees1111 <- remove_burnin(trees1111, 0.9)
 consensus_tree1111 <- myconsensus(trees1111)
 plot(consensus_tree1111, main = "1111")
-write.tree(consensus_tree1111, here(here("output/by_level/loom1111/consensus_tree1111.tree")))
+write.tree(consensus_tree1111, here("output/by_level/loom1111/consensus_tree1111.tree"))
 
 
 # Weigth 8421 : Burnin 90%
@@ -26,7 +26,7 @@ trees8421 <- read.nexus(path8421)
 trees8421 <- remove_burnin(trees8421, 0.9)
 consensus_tree8421 <- myconsensus(trees8421)
 plot(consensus_tree8421, main = "8421")
-write.tree(consensus_tree1111, here(here("output/by_level/loom8421/consensus_tree8421.tree")))
+write.tree(consensus_tree1111, here("output/by_level/loom8421/consensus_tree8421.tree"))
 
 # Weigth 1000 : Burnin 90%
 path1000 <- here("output/by_level/loom1000/kd_loom1000.trees")
@@ -34,7 +34,7 @@ trees1000 <- read.nexus(path1000)
 trees1000 <- remove_burnin(trees1000, 0.9)
 consensus_tree1000 <- myconsensus(trees1000)
 plot(consensus_tree1000, main = "1000")
-write.tree(consensus_tree1000, here(here("output/by_level/loom1000/consensus_tree1000.tree")))
+write.tree(consensus_tree1000, here("output/by_level/loom1000/consensus_tree1000.tree"))
 
 
 
