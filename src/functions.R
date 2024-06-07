72dc6b4a03859681d38130035888d8e1b681d63d
Emma <emma.kopp@dauphine.psl.eu>
2024-06-07 17:00:10 +0200

    tidy tentative

--- src/functions.R
+++ src/functions.R
@@ -1,99 +1,75 @@
 library(here)
 
-duplication_with_option = function(weights,option,path){
+duplication <- function(weights, path) {
   # Read data
-  kd_xl = read_xls(path)
-  rownames = row.names(kd_xl)
-  kd_xl = as.data.frame(sapply(kd_xl,as.numeric))
-  row.names(kd_xl) = rownames
-  
-  # Create data frames 
-  if (option=='level'){data = create_kd_by_level_df(path)}
-  
+  kd_xl <- read_excel(path, sheet = "Digit", skip = 1) %>%
+    select(-starts_with("...")) %>%
+    mutate(Character = str_replace_all(Character, " ", "_")) %>%
+    column_to_rownames("Character") %>%
+    sapply(as.numeric) %>%
+    as.data.frame(stringsAsFactors = FALSE) %>%
+    rownames_to_column(var = "Character") %>%
+    column_to_rownames("Character")
+
+  # Create data frames
+  data <- create_kd_by_level_df(path)
+
   # Duplication
-  kd_xl_weighted = duplicate_looms(
-    weights=weights,
-    option=option,
-    data=data)
-  
+  kd_xl_weighted <- duplicate_looms(weights, data)
+
   return(kd_xl_weighted)
 }
 
-read_xls = function(path){
+create_kd_by_level_df <- function(path) {
+  kd_xls <- read_xls(path)
 
-  kd_xls <- read_excel(path, sheet = "Digit", skip = 1) %>%
-    select(-starts_with("...")) %>%
-    mutate(Character = str_replace_all(Character, " ", "_"))%>%
-    column_to_rownames("Character")
-  
-  return(kd_xls)
-  }
-
-create_kd_by_level_df = function(path){
-
-  kd_xls = read_xls(path)
-  
   # Instanciate a dataframe for a given level i
-  kd_to_level = function(i,kd_xls){
+  kd_to_level <- function(i, kd_xls) {
     kd_level <- kd_xls %>%
       t() %>%
       as.data.frame() %>%
       filter(Level == i) %>%
       select(-Level) %>%
       t() %>%
-      as.data.frame() 
+      as.data.frame()
     return(kd_level)
   }
-  
+
   # Create four data-frames one for each level
-  for (i in 1:4){
-    var_name = paste0("kd_level", i)
-    assign(var_name, kd_to_level(i,kd_xls), envir = .GlobalEnv)
+  for (i in 1:4) {
+    var_name <- paste0("kd_level", i)
+    assign(var_name, kd_to_level(i, kd_xls), envir = .GlobalEnv)
   }
-  data = list(kd_level1 =kd_level1,
-              kd_level2 =kd_level2,
-              kd_level3 =kd_level3,
-              kd_level4 =kd_level4)
-  
-  return(data)
-  }
-
-duplicate_looms = function(weights,option,data){
-
-  if (option == "level"){
-    file_names <- c("kd_level1", "kd_level2", "kd_level3","kd_level4")
-  }
-  
-  if (option == "type"){
-    file_names = c("basic","simple","complex")
-  }
-  
-  kd_xl_weighted <- do.call(
-    cbind, 
-    lapply(seq_along(weights), 
-           function(i) duplicate_df(data[[file_names[i]]], weights[i]))
+  data <- list(
+    kd_level1 = kd_level1,
+    kd_level2 = kd_level2,
+    kd_level3 = kd_level3,
+    kd_level4 = kd_level4
   )
+
+  return(data)
+}
+
+
+duplicate_looms <- function(weights, data) {
+  file_names <- c("kd_level1", "kd_level2", "kd_level3", "kd_level4")
   
-  return(kd_xl_weighted)
+  seq_along(weights) %>%
+    lapply(function(i) {
+      data[[file_names[i]]] %>% duplicate_df(weights[i])
+    }) %>%
+    do.call(cbind, .)
 }
 
-duplicate_df = function(data, N) {
-  #'@description
-  #'This function duplicate a dataframe n times
-  return(data[,rep(seq_len(ncol(data)), N) ])
+duplicate_df <- function(data, N) {
+  return(data[, rep(seq_len(ncol(data)), N)])
 }
 
-duplicate_df = function(data, N) {
-  #'@description
-  #'This function duplicate a dataframe n times
-  return(data[,rep(seq_len(ncol(data)), N) ])
-}
+read_xls <- function(path) {
+  kd_xls <- read_excel(path, sheet = "Digit", skip = 1) %>%
+    select(-starts_with("...")) %>%
+    mutate(Character = str_replace_all(Character, " ", "_")) %>%
+    column_to_rownames("Character")
 
-data_to_nexus = function(df,path_out){
-
-  df <- df |>
-    as.matrix() |>
-    MatrixToPhyDat()
-  
-  write.phyDat(df, path_out, format = "nexus")
+  return(kd_xls)
 }
--- src/weight_db.R
+++ src/weight_db.R
@@ -1,6 +1,5 @@
 # Import file
 library(here)
-source(here("src/functions.R"))
 library(stringr)
 library(tidyverse)
 library(readxl)
@@ -10,16 +9,18 @@ library(dplyr)
 library(ape)
 library(phytools)
 
+source(here("src/functions.R"))
 
 # Init
 path <- here("data/Kra-DaiLooms28master-2.xlsx")
-weight_level <- c(30, 0, 0, 0)
+weight_level <- c(5, 0, 0, 0)
 output_path <- paste0("output/NexusFiles/", "looms_", paste(weight_level, collapse = ""), ".nex")
 
-kd_xl_weighted <- duplication_with_option(
-  weights = weight_level,
-  option = "level",
-  path = path
-)
+kd_xl_weighted <- duplication(weights = weight_level,path = path)
 
-data_to_nexus(kd_xl_weighted, output_path)
+# Export
+kd_xl_weighted <- kd_xl_weighted |>
+  as.matrix() |>
+  MatrixToPhyDat()
+
+write.phyDat(kd_xl_weighted, output_path, format = "nexus")
