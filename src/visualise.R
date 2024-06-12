d41e9559c2a8815a3737ec7ee45b0c1ecbba39f9
tpellard <thomas.pellard@cnrs.fr>
2024-06-12 18:34:01 +0200

    Update visualise.R

--- src/visualise.R
+++ src/visualise.R
@@ -36,3 +36,22 @@ cs_tree1000_edges |>
   theme(plot.margin = margin(0, 2.5, 0, 0, unit = "line"), aspect.ratio = 1)
 ggsave(here("output/figures/cs_tree1000.pdf"), device = cairo_pdf, width = wd, height = wd, units = "cm")
 plot_crop(here("output/figures/cs_tree1000.pdf"))
+
+tree1111 <- read.nexus(here("data/by_level/loom1111/kd_loom1111.trees"))
+cs_tree1111 <- consensus(tree1111, p = .5, rooted = TRUE)
+cs_tree1111_edges <- consensus.edges(tree1111, consensus.tree = cs_tree1111, rooted = TRUE)
+cs_tree1111_edges$root.edge <- 0
+cs_tree1111_edges$node.label <- round(as.numeric(cs_tree1111_edges$node.label), 2) * 100
+cs_tree1111_edges$node.label[1] <- NA
+cs_tree1111_edges$tip.label <- str_replace_all(cs_tree1111_edges$tip.label, "_", " ")
+cs_tree1111_edges$edge.length <- cs_tree1111_edges$edge.length[-length(cs_tree1111_edges$edge.length)]
+
+cs_tree1111_edges |> 
+  ggtree(ladderize = TRUE) +
+  geom_tiplab(family = base_font, size = base_font_size / .pt) +
+  geom_nodelab(family = base_font, size = (base_font_size - 1) / .pt, hjust = 1.5, vjust = -.5) +
+  geom_rootedge(.25) +
+  coord_cartesian(clip = "off", expand = FALSE) +
+  theme(plot.margin = margin(0, 2.5, 0, 0, unit = "line"), aspect.ratio = 1)
+ggsave(here("output/figures/cs_tree1111.pdf"), device = cairo_pdf, width = wd, height = wd, units = "cm")
+plot_crop(here("output/figures/cs_tree1111.pdf"))
