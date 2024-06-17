9cba3b35fc03e09f8fcb38022b788173e2315d02
Thomas Pellard <thomas.pellard@cnrs.fr>
2024-06-17 10:39:27 +0200

    Get the consensus language tree

--- src/Transform.R
+++ src/Transform.R
@@ -5,28 +5,40 @@ library(phytools)
 # library(treeio)
 
 dir.create(here("output/trees"))
+ntrees <- 1000
+
+
+# Consensus trees for looms ---------------------------------------------------------------------------------------
 
 tree1000 <- read.nexus(here("data/by_level/loom1000/kd_loom1000.trees"))
-tree1000 <- tree1000[seq(2, length(tree1000), by = round(length(tree1000) / 1000))]
+tree1000 <- tree1000[seq(2, length(tree1000), by = round(length(tree1000) / ntrees))]
 cs_tree1000 <- consensus(tree1000, p = .5, rooted = TRUE)
 cs_tree1000 <- consensus.edges(tree1000, consensus.tree = cs_tree1000, rooted = TRUE)
 write.tree(cs_tree1000, here("output/trees/kd_loom1000_consensus.tree"))
 
 tree1111 <- read.nexus(here("data/by_level/loom1111/kd_loom1111.trees"))
-tree1111 <- tree1111[seq(2, length(tree1111), by = round(length(tree1111) / 1000))]
+tree1111 <- tree1111[seq(2, length(tree1111), by = round(length(tree1111) / ntrees))]
 cs_tree1111 <- consensus(tree1111, p = .5, rooted = TRUE)
 cs_tree1111 <- consensus.edges(tree1111, consensus.tree = cs_tree1111, rooted = TRUE)
 write.tree(cs_tree1111, here("output/trees/kd_loom1111_consensus.tree"))
 
 tree8421 <- read.nexus(here("data/by_level/loom8421/kd_loom8421.trees"))
-tree8421 <- tree8421[seq(2, length(tree8421), by = round(length(tree8421) / 1000))]
+tree8421 <- tree8421[seq(2, length(tree8421), by = round(length(tree8421) / ntrees))]
 cs_tree8421 <- consensus(tree8421, p = .5, rooted = TRUE)
 cs_tree8421 <- consensus.edges(tree8421, consensus.tree = cs_tree8421, rooted = TRUE)
 write.tree(cs_tree8421, here("output/trees/kd_loom8421_consensus.tree"))
 
 
+# Consensus tree for languages ------------------------------------------------------------------------------------
+
 lgs_trees <- read.nexus(here("data/languages/kd2.trees"))
-lgs_trees <- lgs_trees[seq(2, length(lgs_trees), by = round(length(lgs_trees) / 1000))]
+lgs_trees <- lgs_trees[seq(2, length(lgs_trees), by = round(length(lgs_trees) / ntrees))]
+kd_lgs_cs <- consensus(lgs_trees, p = .5, rooted = TRUE)
+kd_lgs_cs <- consensus.edges(lgs_trees, consensus.tree = kd_lgs_cs, rooted = TRUE)
+write.tree(kd_lgs_cs, here("output/trees/kd_lgs_consensus.tree"))
+
+
+# Ages in languages trees  ----------------------------------------------------------------------------------------
 
 getMRCA_age <- function(tree, tips) {
   tips <- if (is.character(tips)) which(tree$tip.label %in% tips) else tips
