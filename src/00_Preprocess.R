ec10d8a95d1456653e2fb9f674235a0e86aad4e0
Thomas Pellard <thomas.pellard@cnrs.fr>
2024-06-27 10:20:04 +0200

    Update 00_Preprocess.R

--- src/00_Preprocess.R
+++ src/00_Preprocess.R
@@ -56,23 +56,27 @@ kd_looms_matrix_weighted |>
 
 k <- 5000
 
-kd_lgs_bcov <- read.nexus(here("data/raw_trees/kd-lgs_bcov.trees"))
+kd_lgs_bcov <- read.nexus(here("data/kd-lgs/kd-lgs_bcov/kd-lgs_bcov.trees"))
 kd_lgs_bcov_trimmed <- kd_lgs_bcov[seq(from = 2, to = length(kd_lgs_bcov), by = (length(kd_lgs_bcov) - 1) / k)]
 write.tree(kd_lgs_bcov_trimmed, here("data/kd-lgs/kd-lgs_bcov/kd-lgs_bcov_trimmed.trees"))
 
-kd_looms_bcov1000 <- read.nexus(here("data/raw_trees/kd-looms_bcov1000.trees"))
+kd_looms_bcov1000 <- read.nexus(here("data/kd-looms/kd-looms_bcov1000/kd-looms_bcov1000.trees"))
 kd_looms_bcov1000_trimmed <- kd_looms_bcov1000[seq(from = 2, to = length(kd_looms_bcov1000), by = (length(kd_looms_bcov1000) - 1) / k)]
 write.tree(kd_looms_bcov1000_trimmed, here("data/kd-looms/kd-looms_bcov1000/kd-looms_bcov1000_trimmed.trees"))
 
-kd_looms_bcov1111 <- read.nexus(here("data/raw_trees/kd-looms_bcov1111.trees"))
+kd_looms_bcov1111 <- read.nexus(here("data/kd-looms/kd-looms_bcov1111/kd-looms_bcov1111.trees"))
 kd_looms_bcov1111_trimmed <- kd_looms_bcov1111[seq(from = 2, to = length(kd_looms_bcov1111), by = (length(kd_looms_bcov1111) - 1) / k)]
 write.tree(kd_looms_bcov1111_trimmed, here("data/kd-looms/kd-looms_bcov1111/kd-looms_bcov1111_trimmed.trees"))
 
-kd_looms_bcov8421 <- read.nexus(here("data/raw_trees/kd-looms_bcov8421.trees"))
+kd_looms_bcov8421 <- read.nexus(here("data/kd-looms/kd-looms_bcov8421/kd-looms_bcov8421.trees"))
 kd_looms_bcov8421_trimmed <- kd_looms_bcov8421[seq(from = 2, to = length(kd_looms_bcov8421), by = (length(kd_looms_bcov8421) - 1) / k)]
 write.tree(kd_looms_bcov8421_trimmed, here("data/kd-looms/kd-looms_bcov8421/kd-looms_bcov8421_trimmed.trees"))
 
-kd_looms_ctmc6 <- read.nexus(here("data/raw_trees/kd-looms_ctmc6.trees"))
+kd_looms_ctmc4 <- read.nexus(here("data/kd-looms/kd-looms_ctmc4/kd-looms_ctmc4.trees"))
+kd_looms_ctmc4_trimmed <- kd_looms_ctmc4[seq(from = 2, to = length(kd_looms_ctmc4), by = (length(kd_looms_ctmc4) - 1) / k)]
+write.tree(kd_looms_ctmc4_trimmed, here("data/kd-looms/kd-looms_ctmc4/kd-looms_ctmc4_trimmed.trees"))
+
+kd_looms_ctmc6 <- read.nexus(here("data/kd-looms/kd-looms_ctmc6/kd-looms_ctmc6.trees"))
 kd_looms_ctmc6_trimmed <- kd_looms_ctmc6[seq(from = 2, to = length(kd_looms_ctmc6), by = (length(kd_looms_ctmc6) - 1) / k)]
 write.tree(kd_looms_ctmc6_trimmed, here("data/kd-looms/kd-looms_ctmc6/kd-looms_ctmc6_trimmed.trees"))
 
