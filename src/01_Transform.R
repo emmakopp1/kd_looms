3e247c1cbb3845b52d1e3bfeced5741f4a18373f
Thomas Pellard <thomas.pellard@cnrs.fr>
2024-07-10 11:55:56 +0200

    Update 01_Transform.R

--- src/01_Transform.R
+++ src/01_Transform.R
@@ -9,46 +9,87 @@ dir.create(here("output/trees"))
 dir.create(here("output/data"))
 
 
-# Consensus trees -------------------------------------------------------------------------------------------------
+# Consensus trees --------------------------------------------------------------
 
 burnin <- .1
 
 tmp <- tempdir()
-unzip(here("data/kd-lgs/kd-lgs_bcov/kd-lgs_bcov.trees.zip"), junkpaths = TRUE, exdir = tmp)
+unzip(here("data/kd-lgs/kd-lgs_bcov/kd-lgs_bcov.trees.zip"),
+  junkpaths = TRUE,
+  exdir = tmp
+)
 kd_lgs_bcov <- read.nexus(paste0(tmp, "/kd-lgs_bcov.trees"))
 kd_lgs_bcov <- kd_lgs_bcov[ceiling(length(kd_lgs_bcov) * burnin):length(kd_lgs_bcov)]
 kd_lgs_bcov_cs <- consensus(kd_lgs_bcov, p = .5, rooted = TRUE)
-kd_lgs_bcov_cs <- consensus.edges(kd_lgs_bcov, consensus.tree = kd_lgs_bcov_cs, rooted = TRUE)
+kd_lgs_bcov_cs <- consensus.edges(kd_lgs_bcov,
+  consensus.tree = kd_lgs_bcov_cs,
+  rooted = TRUE
+)
 kd_lgs_bcov_cs$root.edge.length <- 0
-write.tree(kd_lgs_bcov_cs, here("output/trees/kd-lgs_bcov_consensus.tree"))
+write.tree(
+  kd_lgs_bcov_cs,
+  here("output/trees/kd-lgs_bcov_consensus.tree")
+)
 unlink(tmp, recursive = TRUE)
 
-kd_looms_bcov1000 <- read.nexus(here("data/kd-looms/kd-looms_bcov1000/kd-looms_bcov1000.trees"))
+kd_looms_bcov1000 <- read.nexus(
+  here("data/kd-looms/kd-looms_bcov1000/kd-looms_bcov1000.trees")
+)
 kd_looms_bcov1000 <- kd_looms_bcov1000[ceiling(length(kd_looms_bcov1000) * burnin):length(kd_looms_bcov1000)]
 kd_looms_bcov1000_cs <- consensus(kd_looms_bcov1000, p = .5, rooted = TRUE)
-kd_looms_bcov1000_cs <- consensus.edges(kd_looms_bcov1000, consensus.tree = kd_looms_bcov1000_cs, rooted = TRUE)
-write.tree(kd_looms_bcov1000_cs, here("output/trees/kd-looms_bcov1000_consensus.tree"))
+kd_looms_bcov1000_cs <- consensus.edges(kd_looms_bcov1000,
+  consensus.tree = kd_looms_bcov1000_cs,
+  rooted = TRUE
+)
+write.tree(
+  kd_looms_bcov1000_cs,
+  here("output/trees/kd-looms_bcov1000_consensus.tree")
+)
 
-kd_looms_bcov1111 <- read.nexus(here("data/kd-looms/kd-looms_bcov1111/kd-looms_bcov1111.trees"))
+kd_looms_bcov1111 <- read.nexus(
+  here("data/kd-looms/kd-looms_bcov1111/kd-looms_bcov1111.trees")
+)
 kd_looms_bcov1111 <- kd_looms_bcov1111[ceiling(length(kd_looms_bcov1111) * burnin):length(kd_looms_bcov1111)]
 kd_looms_bcov1111_cs <- consensus(kd_looms_bcov1111, p = .5, rooted = TRUE)
-kd_looms_bcov1111_cs <- consensus.edges(kd_looms_bcov1111, consensus.tree = kd_looms_bcov1111_cs, rooted = TRUE)
-write.tree(kd_looms_bcov1111_cs, here("output/trees/kd-looms_bcov1111_consensus.tree"))
+kd_looms_bcov1111_cs <- consensus.edges(kd_looms_bcov1111,
+  consensus.tree = kd_looms_bcov1111_cs,
+  rooted = TRUE
+)
+write.tree(
+  kd_looms_bcov1111_cs,
+  here("output/trees/kd-looms_bcov1111_consensus.tree")
+)
 
-kd_looms_bcov8421 <- read.nexus(here("data/kd-looms/kd-looms_bcov8421/kd-looms_bcov8421.trees"))
+kd_looms_bcov8421 <- read.nexus(
+  here("data/kd-looms/kd-looms_bcov8421/kd-looms_bcov8421.trees")
+)
 kd_looms_bcov8421 <- kd_looms_bcov8421[ceiling(length(kd_looms_bcov8421) * burnin):length(kd_looms_bcov8421)]
 kd_looms_bcov8421_cs <- consensus(kd_looms_bcov8421, p = .5, rooted = TRUE)
-kd_looms_bcov8421_cs <- consensus.edges(kd_looms_bcov8421, consensus.tree = kd_looms_bcov8421_cs, rooted = TRUE)
-write.tree(kd_looms_bcov8421_cs, here("output/trees/kd-looms_bcov8421_consensus.tree"))
+kd_looms_bcov8421_cs <- consensus.edges(kd_looms_bcov8421,
+  consensus.tree = kd_looms_bcov8421_cs,
+  rooted = TRUE
+)
+write.tree(
+  kd_looms_bcov8421_cs,
+  here("output/trees/kd-looms_bcov8421_consensus.tree")
+)
 
-kd_looms_ctmc4 <- read.nexus(here("data/kd-looms/kd-looms_ctmc4/kd-looms_ctmc4.trees"))
+kd_looms_ctmc4 <- read.nexus(
+  here("data/kd-looms/kd-looms_ctmc4/kd-looms_ctmc4.trees")
+)
 kd_looms_ctmc4 <- kd_looms_ctmc4[ceiling(length(kd_looms_ctmc4) * burnin):length(kd_looms_ctmc4)]
 kd_looms_ctmc4_cs <- consensus(kd_looms_ctmc4, p = .5, rooted = TRUE)
-kd_looms_ctmc4_cs <- consensus.edges(kd_looms_ctmc4, consensus.tree = kd_looms_ctmc4_cs, rooted = TRUE)
-write.tree(kd_looms_ctmc4_cs, here("output/trees/kd-looms_ctmc4_consensus.tree"))
+kd_looms_ctmc4_cs <- consensus.edges(kd_looms_ctmc4,
+  consensus.tree = kd_looms_ctmc4_cs,
+  rooted = TRUE
+)
+write.tree(
+  kd_looms_ctmc4_cs,
+  here("output/trees/kd-looms_ctmc4_consensus.tree")
+)
 
 
-# Ages in language trees --------------------------------------------------
+# Ages in language trees -------------------------------------------------------
 
 getMRCA_age <- function(tree, tips) {
   tips <- if (is.character(tips)) which(tree$tip.label %in% tips) else tips
@@ -61,8 +102,14 @@ kd_lgs_ages <- kd_lgs_bcov |>
   seq_along() |>
   map_df(~ tibble(
     `Kra-Dai` = getMRCA_age(kd_lgs_bcov[[.x]], kd_lgs_bcov[[1]]$tip.label),
-    `Kam-Tai` = getMRCA_age(kd_lgs_bcov[[.x]], str_subset(kd_lgs_bcov[[1]]$tip.label, "^(Ks|Tc|Tn|Tsw)")),
-    `Tai-Yay` = getMRCA_age(kd_lgs_bcov[[.x]], str_subset(kd_lgs_bcov[[1]]$tip.label, "^(Tc|Tn|Tsw)"))
+    `Kam-Tai` = getMRCA_age(
+      kd_lgs_bcov[[.x]],
+      str_subset(kd_lgs_bcov[[1]]$tip.label, "^(Ks|Tc|Tn|Tsw)")
+    ),
+    `Tai-Yay` = getMRCA_age(
+      kd_lgs_bcov[[.x]],
+      str_subset(kd_lgs_bcov[[1]]$tip.label, "^(Tc|Tn|Tsw)")
+    )
   )) |>
   pivot_longer(everything(), names_to = "group", values_to = "age")
 
@@ -70,7 +117,13 @@ write_csv(kd_lgs_ages, here("output/data/kd-lgs_ages.csv"))
 
 kd_lgs_ages_summary <- kd_lgs_ages |>
   group_by(group) |>
-  summarise(mean = mean(age), median = median(age), sd = sd(age), HPDI_lower = hdi(age)["lower"], HPDI_upper = hdi(age)["upper"]) |>
+  summarise(
+    mean = mean(age),
+    median = median(age),
+    sd = sd(age),
+    HPDI_lower = hdi(age)["lower"],
+    HPDI_upper = hdi(age)["upper"]
+  ) |>
   left_join(tribble(
     ~group, ~n,
     "Kam-Tai", length(str_subset(kd_lgs_bcov[[1]]$tip.label, "^(Ks|Tc|Tn|Tsw)")),
@@ -84,15 +137,23 @@ kd_lgs_ages_summary <- kd_lgs_ages |>
 write_csv(kd_lgs_ages_summary, here("output/data/kd-lgs_ages_summary.csv"))
 
 
-# Mutation rates --------------------------------------------------------------------------------------------------
+# Mutation rates ---------------------------------------------------------------
 
 kd_looms_characters <- read_csv(here("data/kd-looms/kd-looms_characters.csv")) |>
   select(code, level)
 burnin <- .1
 
-kd_looms_mu_bylevel <- parse_beast_tracelog_file(here("data/kd-looms/kd-looms_ctmc4/kd-looms_ctmc4.log")) |>
+kd_looms_mu_bylevel <- parse_beast_tracelog_file(
+  here("data/kd-looms/kd-looms_ctmc4/kd-looms_ctmc4.log")
+) |>
   as_tibble() |>
-  select(Sample, mutationRate.s.level1, mutationRate.s.level2, mutationRate.s.level3, mutationRate.s.level4)
+  select(
+    Sample,
+    mutationRate.s.level1,
+    mutationRate.s.level2,
+    mutationRate.s.level3,
+    mutationRate.s.level4
+  )
 
 kd_looms_mu_bylevel_tb <- kd_looms_mu_bylevel |>
   rowid_to_column() |>
@@ -106,7 +167,13 @@ write_csv(kd_looms_mu_bylevel_tb, here("output/data/kd-looms_mu_bylevel.csv"))
 
 kd_looms_mu_summary <- kd_looms_mu_bylevel_tb |>
   group_by(level) |>
-  summarise(mean = mean(rate), median = median(rate), sd = sd(rate), HPDI_lower = hdi(rate)["lower"], HPDI_upper = hdi(rate)["upper"]) |>
+  summarise(
+    mean = mean(rate),
+    median = median(rate),
+    sd = sd(rate),
+    HPDI_lower = hdi(rate)["lower"],
+    HPDI_upper = hdi(rate)["upper"]
+  ) |>
   left_join(count(kd_looms_characters, level)) |>
   relocate(n, .after = level) |>
   rename(n_chars = n)
