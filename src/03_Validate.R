b0bbc781fa6cd89412686bf4f5aa5947314ac646
Emma <emma.kopp@dauphine.psl.eu>
2024-10-30 09:16:54 +0000

    deleted files

--- src/03_Validate.R
+++ src/03_Validate.R
@@ -13,11 +13,18 @@ library(purrr)
 
 # Import data ------------------------------------------------------------------
 
-lgs_to_looms = read_excel(
-  here("data/Kra-DaiLooms.xlsx"),
-  skip = 5,
-  sheet=1) |> 
-  select(Looms = 2, KD = 3)
+kd_looms <- read_csv(here("data/kd-looms/kd-looms_datapoints.csv")) |>
+  select(group, lng, lng_group_code) |>
+  filter(!is.na(lng_group_code)) |>
+  mutate(lng_label = paste0(str_replace_na(lng_group_code, ""), lng)) |>
+  mutate(group = str_replace_all(group, " ", "_"))
+kd_lgs <- read_csv(here("data/kd-lgs/kd-lgs_datapoints.csv")) |>
+  select(lng, lng_group_code) |>
+  filter(!is.na(lng_group_code)) |>
+  mutate(lng_label = paste0(str_replace_na(lng_group_code, ""), lng))
+kd_lgs_looms <- inner_join(kd_looms, kd_lgs) 
+  #mutate(across(where(is.character), ~ str_replace_all(., "Chiangmai", "Chengmai"))) |>
+  #mutate(lng = if_else(row_number() == 10, "BeChengmaiMY", lng))
 
 
 # Functions --------------------------------------------------------------------
@@ -40,18 +47,6 @@ prune_trees <- function(trees) {
 }
 
 
-# Change tip labels of the looms according into the corresponding language
-change_tip_label <- function(tiplabel){
-  L = length(tiplabel)
-  for (l in 1:L){
-    match <- which(str_detect(lgs_to_looms$KD, tiplabel[l]))
-    if (length(match) > 0) {
-      tiplabel[l] <- lgs_to_looms$Looms[match]
-    }}
-  return(tiplabel)
-  }
-  
-
 # ------------- Trees -----------------------
 # Languages
 phylo_lg = readNexus(here("data/kd-lgs/kd-lgs_bcov_relaxed/kd-lgs_bcov_relaxed.trees"))
@@ -67,78 +62,61 @@ phylo_lg2 = phylo_lg[[1]]
 phylo_looms2 = phylo_looms[[1]]
 
 
-# Change tips labels
-# Languages
-for (i in 1:length(phylo_lg)) {
-  tree <- phylo_lg[[i]] 
-  new_labels <- change_tip_label(tree$tip.label)
-  tree$tip.label = new_labels
-  phylo_lg2[[i]] <- tree
-}
-
-# Looms
-for (i in 1:length(phylo_looms)) {
-  tree <- phylo_looms[[i]] 
-  new_labels <- tree |> TipLabels() |> str_remove_all("_")
-  tree$tip.label = new_labels
-  phylo_looms2[[i]] <- tree
-}
-
 # Common taxa
-common_leaves <- intersect(phylo_looms2[[1]]$tip.label, phylo_lg2[[1]]$tip.label)
+tip_to_keep_lgs = intersect(phylo_lg2$tip.label,kd_lgs_looms$lng_label)
+tip_to_keep_looms = intersect(phylo_looms2$tip.label,kd_lgs_looms$group)
 
-for (i in 1:length(phylo_lg)){
-  phylo_lg2[[i]] = keep.tip(phylo_lg2[[i]],common_leaves)
+keep.tip(phylo_lg[[1]],tip_to_keep_lgs)
+
+for (i in 2:length(phylo_lg)){
+  phylo_lg2[[i]] = keep.tip(phylo_lg[[i]],tip_to_keep_lgs)
 }
 
-for (i in 1:length(phylo_looms)){
-  phylo_looms2[[i]] = keep.tip(phylo_looms2[[i]],common_leaves)
+for (i in 2:length(phylo_looms)){
+  phylo_looms2[[i]] = keep.tip(phylo_looms[[i]],tip_to_keep_looms)
 }
 
 # Test of both trees elaged
 #par(mfrow=c(1,2))
-#plot(phylo_lg2[[5]])
+#plot(phylo_lg2[[10]])
 #plot(phylo_looms2[[7]])
 
 
 # Nexus -----------------------------------------------------------------------
 # Looms
-data_looms = read.nexus.data(here("data/nexus/kd-looms_1111.nex"))
-data_looms = t(data.frame(data_looms))
+data_looms = read.nexus.data(here("data/nexus/kd-looms_pruned_filtered.nex")) |>
+  as.data.frame() |>
+  t() 
 mode(data_looms) = "integer"
 data_looms = as.data.frame(data_looms)
-rownames(data_looms) = str_remove(rownames(data_looms),'_')
 
-# Ensure the species in the data match the tree's tip labels
-data_looms <- data_looms[match(phylo_looms2[[1]]$tip.label, rownames(data_looms)), ]
+data_looms <- data_looms[match(phylo_looms2[[2]]$tip.label, rownames(data_looms)), ]
 
-# languages
-# Compute consensus tree and load it 
-#trees_lg <- readNexus(here("data/kd-lgs/kd-lgs_bcov_relaxed/kd-lgs_bcov_relaxed.trees"))
-#trees_lg = remove_burnin(trees_lg, 0.9)
-#tree_lg = myconsensus(trees_lg)
-#write.tree(tree_lg,here("/Users/kopp/Documents/kd_looms/output/ConsensusTree/lg_bcov.tree"))
 
-data_lg = read.nexus.data(here("data/nexus/kd-lgs.nex"))
-data_lg = t(data.frame(data_lg))
+# Languages
+data_lg = read.nexus.data(here("data/nexus/kd-lngs_pruned_filtered.nex")) |>
+  as.data.frame() |>
+  t() 
 mode(data_lg) = "integer"
 data_lg = as.data.frame(data_lg)
 
-rownames(data_lg) = rownames(data_lg) |>
+# Ensure the species in the data match the tree's tip labels
+data_lg <- data_lg[match(phylo_lg2[[2]]$tip.label, rownames(data_lg)), ]
+
+
+# ----------- Perform PCA on binary data ----------
+# Name of looms become languages
+rownames(data_looms) = rownames(data_looms) |>
   map_chr(~ {
-    match <- which(str_detect(lgs_to_looms$KD, fixed(.x)))
+    match <- which(str_detect(kd_lgs_looms$group, fixed(.x)))
     
     if (length(match) > 0) {
-      lgs_to_looms$Looms[match]
+      kd_lgs_looms$lng_label[match]
     } else {
       .x
     }
   })
-# Ensure the species in the data match the tree's tip labels
-data_lg <- data_lg[match(phylo_lg2[[1]]$tip.label, rownames(data_lg)), ]
 
-
-# ----------- Perform PCA on binary data ----------
 #looms
 # Using FactoMineR, scale.unit = FALSE prevents scaling of binary variables
 pca_result_looms <- PCA(data_looms, graph = FALSE, scale.unit = FALSE)
@@ -154,33 +132,22 @@ pc1_lg <- pca_result_lg$ind$coord[,1]
 
 
 
-# Blomberg's K for elaged trees
-
-# Data looms / Tree looms
-i_loom_loom = matrix(NA, ncol=2, nrow= length(phylo_looms2))
-
-for (i in 1:length(phylo_looms2)){
-  res <- phylosig(phylo_looms2[[i]], pc1_looms, method = "K", test = TRUE)
-  i_loom_loom[i,] <- c(res$K,res$P)
-}
-
-mean(i_loom_loom[,1])
-mean(i_loom_loom[,2])
-
-# Data lg / Tree lg
-i_lg_lg = matrix(NA, ncol=2, nrow= length(phylo_lg2))
-for (i in 1:length(phylo_lg2)){
-  res <- phylosig(phylo_lg2[[i]], pc1_lg, method = "K", test = TRUE)
-  i_lg_lg[i,] <- c(res$K,res$P)
-}
-
-mean(i_lg_lg[,1])
-mean(i_lg_lg[,2])
-
+# Blomberg's K for pruned trees
 
 # Data lg / Tree looms
+phylo_looms3 = c()
+
+for (i in 1:length(phylo_looms)){
+  match <- which(str_detect(kd_lgs_looms$group,phylo_looms[[i]]$tip.label))
+  phylo_looms3[[1]]$edge = phylo_looms[[1]]$edge
+  phylo_looms3[[1]]$edge.length = phylo_looms[[1]]$edge.length 
+  phylo_looms3[[1]]$Nnode = phylo_looms[[1]]$Nnode
+  phylo_looms3[[1]]$root.edge = phylo_looms[[1]]$root.edge
+  phylo_looms3[[i]]$tip.label = kd_lgs_looms$lng_label[match]
+  }
+
 i_lg_loom = matrix(NA, ncol=2, nrow= length(phylo_looms2))
-for (i in 1:length(phylo_looms2)){
+for (i in 2:length(phylo_looms2)){
   res <- phylosig(phylo_looms2[[i]], pc1_lg, method = "K", test = TRUE)
   i_lg_loom[i,] <- c(res$K,res$P)
 }
@@ -191,17 +158,15 @@ mean(i_lg_loom[,2])
 # Data looms / Tree lg
 i_loom_lg = matrix(NA, ncol=2, nrow= length(phylo_lg2))
 
-for (i in 1:length(phylo_lg2)){
+for (i in 2:length(phylo_lg2)){
   res <- phylosig(phylo_lg2[[i]], pc1_looms, method = "K", test = TRUE)
   i_loom_lg[i,] <- c(res$K,res$P)
 }
 
-mean(i_loom_lg[,1])
-mean(i_loom_lg[,2])
+mean(i_loom_lg[,1], na.rm=T)
+mean(i_loom_lg[,2], na.rm=T)
 
-
-res$sim.K
-
+#res$sim.K
 
 
 
