0d51c31d37918acb36a29d81342c455c66600a16
Thomas Pellard <thomas.pellard@cnrs.fr>
2024-10-30 11:11:52 +0100

    Update tmp.R

--- src/tmp.R
+++ src/tmp.R
@@ -7,67 +7,68 @@ library(ggtree)
 library(tidyverse)
 
 kd_looms <- read_csv(here("data/kd-looms/kd-looms_datapoints.csv")) |>
-  mutate(lng_label = paste0(str_replace_na(lng_group_code, ""), lng)) |> 
+  mutate(lng_label = paste0(str_replace_na(lng_group_code, ""), lng)) |>
   select(group, lng_label)
 
 burnin <- .1
 
-kd_lgs_pruned_tips <- ReadAsPhyDat(here("data/nexus/kd-lgs_pruned.nex")) |> 
-  as_tibble() |> 
+kd_lgs_pruned_tips <- ReadAsPhyDat(here("data/nexus/kd-lgs_pruned.nex")) |>
+  as_tibble() |>
   colnames()
 kd_lgs_phylo <- read.nexus(here("data/kd-lgs/kd-lgs_bcov_relaxed/kd-lgs_bcov_relaxed.trees"))
 kd_lgs_phylo <- kd_lgs_phylo[ceiling(length(kd_lgs_phylo) * burnin):length(kd_lgs_phylo)]
 kd_lgs_phylo <- keep.tip(kd_lgs_phylo, kd_lgs_pruned_tips)
 
-kd_looms_pruned_tips <- ReadAsPhyDat(here("data/nexus/kd-looms_pruned.nex")) |> 
-  as_tibble() |> 
+kd_looms_pruned_tips <- ReadAsPhyDat(here("data/nexus/kd-looms_pruned.nex")) |>
+  as_tibble() |>
   colnames()
 kd_looms_phylo <- read.nexus(
   here("data/kd-looms/kd-looms_bcov1111_strict_heterogene/kd-looms_bcov1111_strict_heterogene.trees")
 )
 kd_looms_phylo <- kd_looms_phylo[ceiling(length(kd_looms_phylo) * burnin):length(kd_looms_phylo)]
-kd_looms_phylo <- map(1:length(kd_looms_phylo), ~ kd_looms_phylo[[.x]] |> 
-      keep.tip(kd_looms_pruned_tips) |> 
-      fortify() |> 
-      mutate(label = str_replace_all(label, "_", " ")) |> 
-      left_join(kd_looms, by = c("label" = "group")) |> 
-      mutate(label = lng_label) |> 
-      as.phylo()
-    ) |> 
+kd_looms_phylo <- map(1:length(kd_looms_phylo), ~ kd_looms_phylo[[.x]] |>
+  keep.tip(kd_looms_pruned_tips) |>
+  fortify() |>
+  mutate(label = str_replace_all(label, "_", " ")) |>
+  left_join(kd_looms, by = c("label" = "group")) |>
+  mutate(label = lng_label) |>
+  as.phylo()) |>
   as.multiPhylo()
 
-pca_looms <- ReadAsPhyDat(here("data/nexus/kd-looms_pruned.nex")) |> 
+pca_looms <- ReadAsPhyDat(here("data/nexus/kd-looms_pruned.nex")) |>
   as_tibble() |>
   rename_with(~ str_replace_all(.x, "_", " ")) |>
   rename(any_of(setNames(kd_looms$group, kd_looms$lng_label))) |>
   mutate(across(everything(), ~ as.numeric(.x))) |>
   as.matrix() |>
-  t() |> 
+  t() |>
   PCA(graph = FALSE, scale.unit = FALSE)
-pc1_looms <- pca_looms$ind$coord[,1]
+pc1_looms <- pca_looms$ind$coord[, 1]
 
-pca_lgs <- ReadAsPhyDat(here("data/nexus/kd-lgs_pruned.nex")) |> 
+pca_lgs <- ReadAsPhyDat(here("data/nexus/kd-lgs_pruned.nex")) |>
   as_tibble() |>
-  mutate(across(everything(), ~ as.numeric(.x))) |> 
+  mutate(across(everything(), ~ as.numeric(.x))) |>
   as.matrix() |>
   t() |>
   PCA(graph = FALSE, scale.unit = FALSE)
-pc1_lgs <- pca_lgs$ind$coord[,1]
+pc1_lgs <- pca_lgs$ind$coord[, 1]
 
+set.seed(30101005)
 kd_lgs_on_looms_k <- map_df(1:length(kd_looms_phylo), function(i) {
   ksig <- phylosig(kd_looms_phylo[[i]], pc1_lgs, method = "K", test = TRUE)
   tibble(k = ksig$K, p = ksig$P)
 })
-
-kd_lgs_on_looms_k |> 
-  summarise(across(everything(), mean))
+kd_lgs_on_looms_k |>
+  write_csv(here("output/data/kd-lgs_on_looms_k.csv"))
 
 kd_looms_on_lgs_k <- map_df(1:length(kd_lgs_phylo), function(i) {
   ksig <- phylosig(kd_lgs_phylo[[i]], pc1_looms, method = "K", test = TRUE)
   tibble(k = ksig$K, p = ksig$P)
 })
+kd_looms_on_lgs_k |>
+  write_csv(here("output/data/kd-looms_on_lgs_k.csv"))
 
-kd_lgs_on_looms_k |> 
+kd_lgs_on_looms_k |>
   summarise(across(everything(), mean))
-
-
+kd_looms_on_lgs_k |>
+  summarise(across(everything(), mean))
