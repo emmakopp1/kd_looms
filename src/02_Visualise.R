d6eee63340f09b83c0ddd9e5d1abfb7f94e9d8aa
Thomas Pellard <thomas.pellard@cnrs.fr>
2024-10-31 10:43:06 +0100

    Update 02_Visualise.R

--- src/02_Visualise.R
+++ src/02_Visualise.R
@@ -203,8 +203,8 @@ kd_lgs_bcov_relaxed_ht_cs_tree_plot <- kd_lgs_bcov_relaxed_ht_cs_tree |>
     aspect.ratio = 2.25,
     plot.margin = margin(0, 3.5, 0, 0, unit = "line")
   )
-kd_lgs_bcov_relaxed_ht_cs_tree_plot <- flip(kd_lgs_bcov_relaxed_ht_cs_tree_plot, 167, 162) |> 
-  flip(167, 154) |> 
+kd_lgs_bcov_relaxed_ht_cs_tree_plot <- flip(kd_lgs_bcov_relaxed_ht_cs_tree_plot, 167, 162) |>
+  flip(167, 154) |>
   flip(105, 125)
 ggsave(here("output/figures/kd-lgs_bcov_relaxed_ht_cs_tree.pdf"),
   kd_lgs_bcov_relaxed_ht_cs_tree_plot,
@@ -249,8 +249,8 @@ kd_lgs_bcov_relaxed_uni_cs_tree_plot <- kd_lgs_bcov_relaxed_uni_cs_tree |>
     plot.margin = margin(0, 3.5, 0, 0, unit = "line")
   )
 ggsave(here("output/figures/kd-lgs_bcov_relaxed_uni_cs_tree.pdf"),
-       kd_lgs_bcov_relaxed_uni_cs_tree_plot,
-       device = cairo_pdf, width = wd, height = wd * 3, units = "cm"
+  kd_lgs_bcov_relaxed_uni_cs_tree_plot,
+  device = cairo_pdf, width = wd, height = wd * 3, units = "cm"
 )
 plot_crop(here("output/figures/kd-lgs_bcov_relaxed_uni_cs_tree.pdf"))
 
@@ -292,8 +292,8 @@ kd_lgs_bcov_strict_uni_cs_tree_plot <- kd_lgs_bcov_strict_uni_cs_tree |>
   )
 kd_lgs_bcov_strict_uni_cs_tree_plot <- flip(kd_lgs_bcov_strict_uni_cs_tree_plot, 127, 108)
 ggsave(here("output/figures/kd-lgs_bcov_strict_uni_cs_tree.pdf"),
-       kd_lgs_bcov_strict_uni_cs_tree_plot,
-       device = cairo_pdf, width = wd, height = wd * 3, units = "cm"
+  kd_lgs_bcov_strict_uni_cs_tree_plot,
+  device = cairo_pdf, width = wd, height = wd * 3, units = "cm"
 )
 plot_crop(here("output/figures/kd-lgs_bcov_strict_uni_cs_tree.pdf"))
 
@@ -578,9 +578,9 @@ kd_lng_tree <- ggtree(kd_cophylo$trees[[1]],
   ladderize = FALSE,
   size = lwd,
   branch.length = "none"
-) |> 
+) |>
   flip(167, 162) |>
-  flip(167, 154) |> 
+  flip(167, 154) |>
   flip(162, 154)
 #   flip(105, 125)
 # flip(113, 128) |>
