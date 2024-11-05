a82a2e215b4e3c4470a2b3dcccc758b7225d68ad
Thomas Pellard <thomas.pellard@cnrs.fr>
2024-11-05 10:30:51 +0100

    Update tmp.R

--- src/tmp.R
+++ src/tmp.R
@@ -25,5 +25,9 @@
 # kd_looms_bcov1111_strict_ht_cs_tree <- read.tree(
 #   here("output/trees/kd-looms_bcov1111_strict_ht_consensus.tree")
 # )
-
+# |>
+#   mutate(model = str_replace(model, "bcov", "binary covarion")) |>
+#   mutate(model = str_replace(model, "ht", "heterogeneous")) |>
+#   mutate(model = str_replace(model, "uni", "uniform")) |>
+#   separate_wider_delim(model, "_", names = c("substitution", "clock", "rate"))
 
